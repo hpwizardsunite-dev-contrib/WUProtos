2b86aff0c49a5a3a63e3768e19ded151fb56591a
--=FurtiF™=-- <Furtif@users.noreply.github.com>
2019-04-24 20:21:15 +0200

    Update decode.bat

--- decode.bat
+++ decode.bat
@@ -1,9 +1,9 @@
     
 @echo off
 
-SET "pogo_protos_path=.\src\"**
+SET "wu_protos_path=.\src\"**
 
-SET "pogo_protos_target=WUProtos.Data.GameDataWrapper"**
-SET "pogo_protos_template=.\src\WUProtos\Data\GameDataWrapper.proto"**
+SET "wu_protos_target=WUProtos.Data.GameDataWrapper"**
+SET "wu_protos_template=.\src\WUProtos\Data\GameDataWrapper.proto"**
 
-protoc --proto_path=%pogo_protos_path% --decode %pogo_protos_target%  %pogo_protos_template%  < %1 > %1_decoded.txt
+protoc --proto_path=%wu_protos_path% --decode %wu_protos_target%  %wu_protos_template%  < %1 > %1_decoded.txt

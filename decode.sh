e42255621edf2499af845cab435acfb4a62ccb18
Furtif <msntchat@hotmail.fr>
2019-04-28 14:06:33 +0200

    test scripts..

--- decode.sh
+++ decode.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 args=("$@")
 
@@ -13,12 +13,11 @@ exit
 echo WUProtos decode tool.
 echo
 
-if  [ "${args[0]}" = "" ]
-then
-{
-echo Usage ${args[0]} [file_to_decode.ext].
-exit
-}
+if  [ "${args[0]}" = "" ]; then
+	echo Usage ${args[0]} [file_to_decode.ext].
+	exit
+fi
+
 
 echo Decoding ${args[0]}. Wait...
 export  wu_protos_path=./src/*
@@ -26,4 +25,3 @@ export  wu_protos_target=WUProtos.Data.GameDataWrapper
 export  wu_protos_template=./src/WUProtos/Data/GameDataWrapper.proto
 protoc --proto_path=$wu_protos_path --decode $wu_protos_target  $wu_protos_template  < ${args[0]} > ${args[0]}_decoded.txt
 echo Decoded file ${args[0]}_decoded.txt
-done
